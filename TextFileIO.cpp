051d6c62d13e2b01d2093729c42b43f99694af9d
hippotipper <hippotipper@gmail.com>
2026-05-02 00:33:25 -0700

    reading from a file

--- TextFileIO.cpp
+++ TextFileIO.cpp
@@ -21,5 +21,32 @@ int main() {
     }
     writeFile.close();
 
+    // other syntax: use ofstream to open file for writing and include file name as parameterd
+    std::ofstream writeFile2("newFile.txt");
+    if (writeFile2.is_open()) {
+        writeFile2 << "See you next time." << std::endl;
+    }
+    writeFile2.close();
+
+    // read a file, spaces will be counted as the end of a line, fstream does not prevent writing to the file
+    std::fstream readFile("newFile.txt", std::ios::in);
+    if (readFile.is_open()) {
+        std::string line;
+        while (readFile >> line) {
+            std::cout << line << std::endl;
+        }
+    }
+    readFile.close();
+
+    std::ifstream readFile2("newFile.txt");
+    if (readFile2.is_open()) {
+        std::string line;
+        // to get the entire line use std::getline()
+        // why is the "l" not capitalized? The world may never know.
+        while (std::getline(readFile2, line)) {
+            std::cout << line << std::endl;
+        }
+    }
+
     return 0;
 }
