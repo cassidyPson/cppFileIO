f4d91e1dc46a59e7f3e25a580711020113e33021
hippotipper <hippotipper@gmail.com>
2026-05-01 23:59:18 -0700

    appending to a file

--- TextFileIO.cpp
+++ TextFileIO.cpp
@@ -3,12 +3,23 @@
 #include <string>
 
 int main() {
+    // initialize a file to write
     std::fstream writeFile;
+    // open the file if it exists, if not create a new one, then select write mode
     writeFile.open("newFile.txt", std::ios::out);
-    std::cout << writeFile.is_open() << std::endl;
+    // if the file is open write a line
     if (writeFile.is_open()) {
-        writeFile << "Hello World." << std::endl;
+        writeFile << "Hello" << std::endl;
+    }
+    // close the file
+    writeFile.close();
+
+    // open the file again and append to it
+    writeFile.open("newFile.txt", std::ios::app);
+    if (writeFile.is_open()) {
+        writeFile << "World." << std::endl;
     }
     writeFile.close();
+
     return 0;
 }
